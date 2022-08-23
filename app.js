dda0eafec4ef4824f8a22a1c3f52f8aee5e4ed0d
Pepp <reels-fencers.0@icloud.com>
2022-08-22 22:03:54 -0400

    for each array method

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('Hello Node!')
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    // this...
+    for (let i=0; i < profileDataArr.length; i++) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log('================');
+
+    // is the same as this...
+    // profileDataArr.forEach((profileItem) => {
+    //     console.log(profileItem)
+    // });
+
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
